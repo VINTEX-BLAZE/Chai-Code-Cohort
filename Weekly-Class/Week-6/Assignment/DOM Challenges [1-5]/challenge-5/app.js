7d2a84c6d65cab814027637f225a4152848e3a7a
Indranil Mukherjee <rpmukherjee@gmail.com>
2025-03-08 23:33:06 +0530

    Solving challange 5

--- Weekly-Class/Week-6/Assignment/DOM Challenges [1-5]/challenge-5/app.js
+++ Weekly-Class/Week-6/Assignment/DOM Challenges [1-5]/challenge-5/app.js
@@ -20,3 +20,55 @@ const images = [
     caption: 'Urban City Skyline',
   },
 ];
+
+// Targeted the next and previous btns
+let nextButton = document.getElementById('nextButton');
+let prevButton = document.getElementById('prevButton');
+let SlideshowBtn = document.getElementById('autoPlayButton')
+let TimerDisplay = document.getElementById('timerDisplay')
+let imgURL = [];
+let imgCaption = [];
+let count = 1;
+
+// created a array of URLs and a array of captions 
+(function ExtractValues() {
+  images.forEach((obj)=> {
+    imgURL.push(obj.url);
+    imgCaption.push(obj.caption);
+  })
+})();
+// console.log(imgURL);
+// console.log(imgCaption);
+
+// used an IIFE to load all the images to the carouselTrack
+(function ShowFirstPic() {
+  imgURL.forEach((URL)=>{
+    img = document.createElement('img')
+    img.setAttribute('src', `${URL}`)
+    img.classList.add('Images')
+    img.style.objectFit = `cover`
+    img.style.width = `100%`
+    img.style.height = `100%`
+    document.getElementById('carouselTrack').appendChild(img)
+  })
+})()
+
+// created a NodeList of all the loaded images
+let IMG_list = document.querySelectorAll('div img');
+
+// Added EventListner to Next btn 
+nextButton.addEventListener('click', (evt)=> {
+  
+})
+
+// added EventListner to Previous btn 
+prevButton.addEventListener('click', (evt)=> {
+  
+})
+
+// added EventListner to SlideshowBtn btn 
+SlideshowBtn.addEventListener('click', (evt)=> {
+  
+})
+
+
